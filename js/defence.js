e1c94c4d0af09ca67effa91868c24ecddbfa5d94
Maxim Kuvardin <maxim@kuvard.in>
2022-05-03 16:30:38 +0600

    Added order books

--- js/defence.js
+++ js/defence.js
@@ -1,5 +1,19 @@
 class DefenceIndicator
 {
+    /**
+     * Number of rows in depth table
+     *
+     * @type {number}
+     */
+    static DEPTH_TABLE_ROWS = 200;
+
+    /**
+     * Depth table step in decipercents
+     *
+     * @type {number}
+     */
+    static DEPTH_TABLE_ROW_STEP = 2;
+
     static MarketsCollection = class {
         /**
          * @type {BinanceStream.Socket}
@@ -16,6 +30,11 @@ class DefenceIndicator
          */
         markets = new Map();
 
+        /**
+         * @type {HTMLDivElement}
+         */
+        orderBooksDiv = document.getElementById('orderBooks');
+
         constructor() {
             this.binanceSocket = new BinanceStream.Socket((message) => {
                 const symbol = message.stream.symbol.toUpperCase();
@@ -24,7 +43,6 @@ class DefenceIndicator
                     console.log(this.markets);
                     return;
                 }
-
 
                 if (message.depthUpdate !== null) {
                     this.markets.get(symbol).processDepthUpdate(message.depthUpdate);
@@ -60,7 +78,11 @@ class DefenceIndicator
                     const tbody = document.getElementById('mainTableBody');
                     const htmlTableRow = tbody.insertRow();
 
-                    const market = new DefenceIndicator.Market(symbol, depth, this.binanceApi, htmlTableRow);
+                    const orderBookDiv = document.createElement('div');
+                    this.orderBooksDiv.append(orderBookDiv);
+
+                    const market = new DefenceIndicator.Market(symbol, depth, this.binanceApi, htmlTableRow,
+                        orderBookDiv);
                     this.markets.set(symbol, market);
                 } catch (exception) {
                     console.log(exception);
@@ -78,6 +100,21 @@ class DefenceIndicator
          * @type {string}
          */
         symbol;
+
+        /**
+         * @type {number|null}
+         */
+        price = null;
+
+        /**
+         * @type {number|null}
+         */
+        bestBidPrice = null;
+
+        /**
+         * @type {number|null}
+         */
+        bestAskPrice = null;
 
         /**
          * @type {Map<number, BinanceStream.DepthItem>}
@@ -117,16 +154,15 @@ class DefenceIndicator
         htmlTableCellValues = new Map();
 
         /**
-         * @private
-         * @type {number}
+         * @type {HTMLElement}
          */
-        _valuesUpdateInterval;
+        htmlPriceElement;
 
         /**
          * @private
          * @type {number}
          */
-        _depthUpdateInterval;
+        valuesUpdateInterval;
 
         /**
          * @type {BinanceApi.Api}
@@ -134,16 +170,85 @@ class DefenceIndicator
         binanceApi;
 
         /**
+         * HTMLDivElement
+         */
+        orderBookDiv;
+
+        /**
+         * @type {HTMLTableCellElement[]}
+         */
+        htmlDepthBidPrices = [];
+
+        /**
+         * @type {HTMLTableCellElement[]}
+         */
+        htmlDepthAskPrices = [];
+
+        /**
+         * @type {HTMLTableCellElement[]}
+         */
+        htmlDepthBidAmounts = [];
+
+        /**
+         * @type {HTMLTableCellElement[]}
+         */
+        htmlDepthAskAmounts = [];
+
+        /**
+         * @type {HTMLTableCellElement[]}
+         */
+        htmlDepthBidAmountSums = [];
+
+        /**
+         * @type {HTMLTableCellElement[]}
+         */
+        htmlDepthAskAmountSums = [];
+
+        /**
+         * @type {number[]}
+         */
+        depthBidAmounts = [];
+
+        /**
+         * @type {number[]}
+         */
+        depthAskAmounts = [];
+
+        /**
+         * @type {number}
+         */
+        symbolsAfterComma = 0;
+
+        depthPrice = null;
+
+
+        /**
          * @param {string} symbol
          * @param {BinanceApi.Depth} depth
          * @param {BinanceApi.Api} binanceApi
          * @param {HTMLTableRowElement} htmlTableRow
+         * @param {HTMLDivElement} orderBookDiv
          */
-        constructor(symbol, depth, binanceApi, htmlTableRow) {
+        constructor(symbol, depth, binanceApi, htmlTableRow, orderBookDiv) {
             this.symbol = symbol;
+
+            depth.bids.forEach((depthBid) => {
+                let priceFraction = depthBid.priceString.split('.')[1];
+                priceFraction = priceFraction.replace(/0+$/g, '');
+                if (priceFraction.length > this.symbolsAfterComma) {
+                    this.symbolsAfterComma = priceFraction.length;
+                }
+            })
+
             this.updateBids(depth.bids);
             this.updateAsks(depth.asks);
+
             this.lastDepthUpdateId = depth.lastUpdateId;
+
+            this.bestAskPrice = Math.min(...Array.from(this.depthAsks.keys()));
+            this.bestBidPrice = Math.max(...Array.from(this.depthBids.keys()));
+
+            this.price = this.bestBidPrice;
 
             this.binanceApi = binanceApi;
             this.htmlTableRow = htmlTableRow;
@@ -160,6 +265,62 @@ class DefenceIndicator
             this.htmlTableCellSpread.classList.add('text-center');
             this.htmlTableCellSpread.classList.add('align-middle');
 
+            this.orderBookDiv = orderBookDiv;
+            this.orderBookDiv.classList.add('col');
+            this.orderBookDiv.classList.add('p-1');
+
+            const orderBookCard = document.createElement('div');
+            orderBookCard.classList.add('card');
+            orderBookCard.classList.add('order-book-card');
+
+            const htmlSymbolElement = document.createElement('span');
+            htmlSymbolElement.innerText = symbol;
+
+            this.htmlPriceElement = document.createElement('span');
+            this.htmlPriceElement.style.float = 'right';
+            this.htmlPriceElement.classList.add('badge');
+            this.htmlPriceElement.innerHTML = this.price.toFixed(this.symbolsAfterComma);
+            this.htmlPriceElement.classList.add('bg-info');
+
+            const orderBookCardHeader = document.createElement('div');
+            orderBookCardHeader.classList.add('card-header');
+            orderBookCardHeader.append(htmlSymbolElement, this.htmlPriceElement);
+
+            const orderBookCardBody = document.createElement('div');
+            orderBookCardBody.classList.add('card-body');
+
+            const depthTable = document.createElement('table');
+            depthTable.classList.add('table');
+            depthTable.classList.add('table-hover');
+            depthTable.classList.add('table-sm');
+
+            for (let i = 1; i <= DefenceIndicator.DEPTH_TABLE_ROWS; i++) {
+                const htmlDepthRow = depthTable.insertRow();
+                const percentsCell = htmlDepthRow.insertCell(0);
+                percentsCell.innerHTML = '<b>' + (i * DefenceIndicator.DEPTH_TABLE_ROW_STEP / 10).toFixed(1) + '%</b>';
+                percentsCell.classList.add('text-end');
+
+                this.htmlDepthBidAmountSums[i] = htmlDepthRow.insertCell(1);
+                this.htmlDepthBidAmounts[i] = htmlDepthRow.insertCell(2);
+                this.htmlDepthBidAmounts[i].classList.add('depth-bid-amount')
+                this.htmlDepthBidAmounts[i].classList.add('text-end')
+
+                this.htmlDepthBidPrices[i] = htmlDepthRow.insertCell(3);
+                this.htmlDepthBidPrices[i].classList.add('text-end');
+
+                this.htmlDepthAskPrices[i] = htmlDepthRow.insertCell(4);
+                this.htmlDepthAskAmounts[i] = htmlDepthRow.insertCell(5);
+                this.htmlDepthAskAmounts[i].classList.add('depth-ask-amount')
+                this.htmlDepthAskAmounts[i].classList.add('text-end')
+
+                this.htmlDepthAskAmountSums[i] = htmlDepthRow.insertCell(6);
+            }
+
+
+            orderBookCardBody.append(depthTable);
+            orderBookCard.append(orderBookCardHeader, orderBookCardBody)
+            this.orderBookDiv.append(orderBookCard)
+
             DefenceIndicator.Market.VALUES_PERCENTS.forEach((percent) => {
                 const cellForPercent = this.htmlTableRow.insertCell();
                 cellForPercent.classList.add('text-center');
@@ -169,18 +330,47 @@ class DefenceIndicator
                 this.htmlTableCellValues.set(percent, cellForPercent);
             })
 
-            this._valuesUpdateInterval = setInterval(() => {
-                const minDeptAskPrice = Math.min(...Array.from(this.depthAsks.keys()));
+            this.valuesUpdateInterval = setInterval(() => {
+                const minDepthAskPrice = Math.min(...Array.from(this.depthAsks.keys()));
                 const maxDepthBidPrice = Math.max(...Array.from(this.depthBids.keys()));
 
-                this.htmlTableCellSpread.innerHTML = `<small style="color: red;">${minDeptAskPrice}</small><br>` +
-                    `<small style="color: green;">${maxDepthBidPrice}</small>`;
+                /**
+                 * @type {number|null}
+                 */
+                let newPrice = null;
+                if (this.bestAskPrice !== null && minDepthAskPrice > this.bestAskPrice) {
+                    newPrice = minDepthAskPrice;
+                } else if (this.bestBidPrice !== null && maxDepthBidPrice < this.bestBidPrice) {
+                    newPrice = maxDepthBidPrice;
+                }
+
+                this.bestAskPrice = minDepthAskPrice;
+                this.bestBidPrice = maxDepthBidPrice;
+
+                if (newPrice !== null) {
+                    this.htmlPriceElement.innerHTML = newPrice.toFixed(this.symbolsAfterComma);
+                    if (newPrice > this.price) {
+                        this.htmlPriceElement.classList.remove('bg-info');
+                        this.htmlPriceElement.classList.remove('bg-danger');
+                        this.htmlPriceElement.classList.add('bg-success');
+                    } else if (newPrice < this.price) {
+                        this.htmlPriceElement.classList.remove('bg-info');
+                        this.htmlPriceElement.classList.remove('bg-success');
+                        this.htmlPriceElement.classList.add('bg-danger');
+                    }
+
+                    this.price = newPrice;
+                }
+
+                this.htmlTableCellSpread.innerHTML =
+                    `<small style="color: red;">${this.bestAskPrice.toFixed(this.symbolsAfterComma)}</small><br>` +
+                    `<small style="color: green;">${this.bestBidPrice.toFixed(this.symbolsAfterComma)}</small>`;
 
                 let depthAsksAmounts = {};
                 let depthBidsAmounts = {};
 
                 DefenceIndicator.Market.VALUES_PERCENTS.forEach((percent) => {
-                    const maxDepthAskPrice = minDeptAskPrice * (1 + percent / 1000);
+                    const maxDepthAskPrice = minDepthAskPrice * (1 + percent / 1000);
                     this.depthAsks.forEach((value) => {
                         if (value.price < maxDepthAskPrice) {
                             if (depthAsksAmounts[percent]) {
@@ -203,9 +393,7 @@ class DefenceIndicator
                     });
                 });
 
-
-
-
+                // Update bulls defence percents
                 DefenceIndicator.Market.VALUES_PERCENTS.forEach((percent) => {
                     const defence = depthAsksAmounts[percent] === 0 && depthBidsAmounts[percent] === 0
                         ? 0
@@ -214,22 +402,100 @@ class DefenceIndicator
 
                     const diff = Math.round((depthBidsAmounts[percent] - depthAsksAmounts[percent]) / 1000);
 
-                    let backgroundOpacity;
+                    const backgroundOpacity = Math.abs(defence - 0.5) * 2;
                     if (defence >= 0.5) {
-                        backgroundOpacity = (defence - 0.5) * 2;
                         this.htmlTableCellValues.get(percent).style.background = `rgba(0, 255, 0, ${backgroundOpacity})`;
                     } else {
-                        backgroundOpacity = (0.5 - defence) * 2;
                         this.htmlTableCellValues.get(percent).style.background = `rgba(255, 0, 0, ${backgroundOpacity})`;
                     }
 
                     const defenceString = Math.round(defence * 100);
-                    this.htmlTableCellValues.get(percent).innerHTML = `${defenceString}<br><small>${diff.toLocaleString()}k</small>`;
+                    this.htmlTableCellValues.get(percent).innerHTML =
+                        `${defenceString}<br><small>${diff.toLocaleString()}k</small>`;
                 });
+
+
+                // Update prices cells
+                if (this.depthPrice === null || this.depthPrice !== this.price) {
+                    for (let i = 1; i <= DefenceIndicator.DEPTH_TABLE_ROWS; i++) {
+                        const depthStepPercents = i * DefenceIndicator.DEPTH_TABLE_ROW_STEP / 10;
+
+                        const depthStepBidPriceMin = this.price * (100 - depthStepPercents) / 100;
+                        const depthStepBidPriceMinString = depthStepBidPriceMin.toFixed(this.symbolsAfterComma);
+                        if (this.htmlDepthBidPrices[i].innerText !== depthStepBidPriceMinString) {
+                            this.htmlDepthBidPrices[i].innerText = depthStepBidPriceMinString;
+                        }
+
+                        const depthStepAskPriceMax = this.price * (100 + depthStepPercents) / 100;
+                        const depthStepAskPriceMaxString = depthStepAskPriceMax.toFixed(this.symbolsAfterComma);
+                        if (this.htmlDepthAskPrices[i].innerText !== depthStepAskPriceMaxString) {
+                            this.htmlDepthAskPrices[i].innerText = depthStepAskPriceMaxString;
+                        }
+                    }
+
+                    this.depthPrice = this.price;
+                }
+
+                /**
+                 * @type {number[]}
+                 */
+                const depthBidAmountsNew = [];
+                this.depthBids.forEach((depthBid) => {
+                    const priceDiffPpm = Math.ceil((this.price - depthBid.price) * 1000 / this.price / DefenceIndicator.DEPTH_TABLE_ROW_STEP);
+                    if (depthBidAmountsNew[priceDiffPpm] === undefined) {
+                        depthBidAmountsNew[priceDiffPpm] = depthBid.amount;
+                    } else {
+                        depthBidAmountsNew[priceDiffPpm] += depthBid.amount;
+                    }
+                });
+
+                const depthAskAmountsNew = [];
+                this.depthAsks.forEach((depthAsk) => {
+                    const priceDiffPpm = Math.ceil((depthAsk.price - this.price) * 1000 / this.price / DefenceIndicator.DEPTH_TABLE_ROW_STEP);
+                    if (depthAskAmountsNew[priceDiffPpm] === undefined) {
+                        depthAskAmountsNew[priceDiffPpm] = depthAsk.amount;
+                    } else {
+                        depthAskAmountsNew[priceDiffPpm] += depthAsk.amount;
+                    }
+                });
+
+
+                let depthBidAmountsSum = 0;
+                let depthAskAmountsSum = 0;
+                for (let i = 1; i <= DefenceIndicator.DEPTH_TABLE_ROWS; i++) {
+                    if (depthBidAmountsNew[i] === undefined) {
+                        depthBidAmountsNew[i] = 0;
+                    }
+
+                    if (depthBidAmountsNew[i] !== this.depthBidAmounts[i]) {
+                        this.depthBidAmounts[i] = depthBidAmountsNew[i];
+                        this.htmlDepthBidAmounts[i].innerText = (this.depthBidAmounts[i] / 1000).toFixed(1);
+                        this.htmlDepthBidAmounts[i].style.backgroundColor = DefenceIndicator.Market.getDepthAmountColor(this.depthBidAmounts[i], true);
+                    }
+
+                    depthBidAmountsSum += depthBidAmountsNew[i];
+                    this.htmlDepthBidAmountSums[i].innerText = Math.floor(depthBidAmountsSum / 1000).toString() + 'k';
+
+                    if (depthAskAmountsNew[i] === undefined) {
+                        depthAskAmountsNew[i] = 0;
+                    }
+
+                    if (depthAskAmountsNew[i] !== this.depthAskAmounts[i]) {
+                        this.depthAskAmounts[i] = depthAskAmountsNew[i];
+                        this.htmlDepthAskAmounts[i].innerText = (this.depthAskAmounts[i] / 1000).toFixed(1);
+                        this.htmlDepthAskAmounts[i].style.backgroundColor = DefenceIndicator.Market.getDepthAmountColor(this.depthAskAmounts[i], false);
+                    }
+
+                    depthAskAmountsSum += depthAskAmountsNew[i];
+                    this.htmlDepthAskAmountSums[i].innerText = Math.floor(depthAskAmountsSum / 1000).toString() + 'k';
+                }
+
             }, 300);
 
-            this._valuesUpdateInterval = setInterval(() => {
-                this.binanceApi.getDepth(this.symbol, 1000).then(depth => {
+
+
+            this.valuesUpdateInterval = setInterval(() => {
+                this.binanceApi.getDepth(this.symbol, 20000).then(depth => {
                     this.lastDepthUpdateId = depth.lastUpdateId;
 
                     this.depthBids.clear();
@@ -244,6 +510,20 @@ class DefenceIndicator
                     //     '<small>' + (new Date()).toLocaleTimeString() + '</small>';
                 });
             }, 20000);
+        }
+
+        static getDepthAmountColor(amount, isBid)
+        {
+            const backgroundOpacity = amount > 100000 ? 1 : amount / 100000;
+            if (amount >= 1000000) {
+                return `rgba(255, 207, 75, ${backgroundOpacity})`;
+            }
+
+            if (isBid) {
+                return `rgba(0, 255, 0, ${backgroundOpacity})`;
+            }
+
+            return `rgba(255, 0, 0, ${backgroundOpacity})`;
         }
 
         /**
@@ -308,17 +588,18 @@ class DefenceIndicator
 const marketsCollection = new DefenceIndicator.MarketsCollection();
 marketsCollection.binanceSocket.socket.onopen = async () => {
     await marketsCollection.addMarket('BTCUSDT');
-    await marketsCollection.addMarket('ETHUSDT');
-    await marketsCollection.addMarket('SOLUSDT');
-    await marketsCollection.addMarket('LUNAUSDT');
-    await marketsCollection.addMarket('DASHUSDT');
-    await marketsCollection.addMarket('LTCUSDT');
-    await marketsCollection.addMarket('ATOMUSDT');
-    await marketsCollection.addMarket('DOTUSDT');
-    await marketsCollection.addMarket('MATICUSDT');
+    // await marketsCollection.addMarket('ETHUSDT');
+    // await marketsCollection.addMarket('SOLUSDT');
+    // await marketsCollection.addMarket('LUNAUSDT');
+    // await marketsCollection.addMarket('DASHUSDT');
+    // await marketsCollection.addMarket('LTCUSDT');
+    // await marketsCollection.addMarket('ATOMUSDT');
+    // await marketsCollection.addMarket('DOTUSDT');
+    // await marketsCollection.addMarket('MATICUSDT');
     await marketsCollection.addMarket('OGNUSDT');
     await marketsCollection.addMarket('MTLUSDT');
     await marketsCollection.addMarket('WAVESUSDT');
+    await marketsCollection.addMarket('AXSUSDT');
 };
 
 function addMarketFromInput() {
