69b906675c28d41fb7dbaf66954656b8d1dda109
Maxim Kuvardin <maxim@kuvard.in>
2022-05-10 18:17:19 +0600

    depthRoundTo turned off

--- js/app.js
+++ js/app.js
@@ -610,7 +610,7 @@ class Dashboard
         this.depthBids = new Map();
         this.minDepthItemAmountToShow = 1000;
         this.depthItemsMaxShow = 50;
-        this.depthRoundTo = 0.0005;
+        this.depthRoundTo = 0;
 
         this.depthUpdateInterval = setInterval(() => {
             this.depthAsks = this.sortDepthItems(this.depthAsks);
